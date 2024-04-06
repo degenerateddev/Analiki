ae040cab8b0de7bbda54e33f29d06534ee506b70
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-04-06 21:13:23 +0200

    Add: visualization blob

---
+++ src/lib/components/structure/Dot.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let title = "";
+    export let text = "";
+    export let links = [];
+    export let iwlinks = [];
+    export let sources = [];
+
+    let showContent = false;
+
+    function stopPropagation(event) {
+        event.stopPropagation();
+    }
+</script>
+
+<button class="relative inline-block rounded-full bg-purple-400 p-6" on:click={() => showContent = !showContent}>
+    <div class="absolute inset-0 rounded-full translate-x-2 translate-y-2 blur" />
+    <div class="relative flex items-center text-center justify-center w-24 h-24 text-sm translate-y-1">
+        {title}
+    </div>
+</button>
+
+{#if showContent}
+    <div class="absolute inset-0 flex flex-col justify-center p-10 bg-purple-900 bg-opacity-50" on:click={() => showContent = !showContent}>
+        <table>
+            <tr>
+              <th>Links</th>
+              <th>IWLinks</th>
+              <th>Sources</th>
+            </tr>
+            <tr>
+                <td>{links.length}</td>
+                <td>{iwlinks.length}</td>
+                <td>{sources.length}</td>
+            </tr>
+        </table>
+        <div class="relative w-[50vw] bg-white overflow-y-scroll" on:click={stopPropagation}>
+            {@html text}
+        </div>
+    </div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
     import { enhance } from '$app/forms';
+    import Dot from '$lib/components/structure/Dot.svelte';
 
-    export let form;    
+    export let form;
 </script>
 
 <main class="container m-auto space-y-10">
@@ -9,15 +10,13 @@
         <input type="text" name="query" class="w-1/3 rounded-full p-3 shadow-lg shadow-purple-500 bg-purple-600 text-white focus:outline-none" />
     </form>
 
-    <div class="text-center bg-stone-800 text-white">
+    <div class="flex justify-center">
         {#if form?.missing}
             <p>Please enter a search query</p>
         {:else if form?.error}
             <p>{form.error}</p>
         {:else if form?.success}
-            <pre class="text-left">
-                {JSON.stringify(form?.data, null, 2)}
-            </pre>
+            <Dot {...form.data} />
         {/if}
     </div>
 </main>
